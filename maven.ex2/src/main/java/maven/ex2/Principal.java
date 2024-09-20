68f53fc2857eae0b39e5b70ce45fd487cfa373af
AayaA1a505 <126629472+AayaA1a505@users.noreply.github.com>
2024-09-19 22:25:45 -0300

    Update Principal.java

--- maven.ex2/src/main/java/maven/ex2/Principal.java
+++ maven.ex2/src/main/java/maven/ex2/Principal.java
@@ -1,8 +1,8 @@
 package maven.ex2;
 
-import java.util.*;
+import java.util.Scanner;
 
-class X {
+class remediosCRUD {
 
     public static void main(String[] args) {
 
@@ -14,32 +14,81 @@ class X {
             System.out.println("====================");
             System.out.println("|       MENU       |");
             System.out.println("|==================|");
-            System.out.println("| 1| Listar itens  |");
-            System.out.println("| 2| Inserir item  |");
-            System.out.println("| 3| Excluir item  |");
-            System.out.println("| 4| Atualizar item|");
+            System.out.println("| 1| Inserir item  |");
+            System.out.println("| 2| Listar itens  |");
+            System.out.println("| 3| Atualizar item|");
+            System.out.println("| 4| Excluir item  |");
             System.out.println("====================");
             System.out.println("Pressione qualquer outro numero para Sair.");
 
             entrada = ler.nextInt();
             switch(entrada) {
                 case 1: {
+                    inserir(ler);
                     break;
                 }
                 case 2: {
-                    break;
+                    listar();
                 }
                 case 3: {
+                    atualizar(ler);
                     break;
                 }
                 case 4: {
+                    excluir(ler);
                     break;
                 }
             }
-
-            System.out.println("Pressione Enter para continuar...");
+            if(entrada < 0 || entrada > 5){
+                System.out.println("Pressione Enter para Sair");
+            }else {
+                System.out.println("Pressione Enter para continuar...");
+            }
+            ler.nextLine();
             ler.nextLine();
         } while(entrada > 0 && entrada < 5);
-        ler.close();
+    }
+    static void inserir(Scanner ler) {
+        String nome;
+        int valor;
+        System.out.println("====================");
+        System.out.println("|   Inserir item   |");
+        System.out.println("|==================|");
+        System.out.println("Insira o nome do item: ");
+        nome = ler.nextLine();
+        System.out.println("Insira o valor do item: ");
+        valor = ler.nextInt();
+       
+
+        //comando pro terminal
+
+        System.out.println("Item inserido com sucesso no banco de dados.");
+        System.out.println("Pressione Enter para continuar...");
+            ler.nextLine();
+            ler.nextLine();
+    }
+    static void listar() {
+        System.out.println("=====================");
+        System.out.println("| listagem de itens |");
+        System.out.println("|===================|");
+
+        DAO.conectar();
+        }
+    static void atualizar(Scanner ler) {
+        int id;
+        System.out.println("====================");
+        System.out.println("|  Atualizar item  |");
+        System.out.println("|==================|");
+        System.out.println("Insira o ID do item: ");
+        id = ler.nextInt();
+        System.out.println("Insira o campo a ser atualizado (preço/nome) do item: ");
+    }
+    static void excluir(Scanner ler) {
+        int id;
+        System.out.println("==================");
+        System.out.println("|  Excluir item  |");
+        System.out.println("|================|");
+        System.out.println("Insira o ID do item: ");
+        id = ler.nextInt();
     }
 }
